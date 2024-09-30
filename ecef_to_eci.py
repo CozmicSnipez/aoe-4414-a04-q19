deef4987544b63e4b5adc1826c2a08e3f96d4e8a
CozmicSnipez <130805541+CozmicSnipez@users.noreply.github.com>
2024-09-30 01:44:25 -0400

    Add files via upload

--- ecef_to_eci.py
+++ ecef_to_eci.py
@@ -32,17 +32,15 @@ def ymdhms_to_jd(year, month, day, hour, minute, second):
     if month <= 2:
         year -= 1
         month += 12
-    A = math.floor(year / 100)
-    B = 2 - A + math.floor(A / 4)
-    jd = math.floor(365.25 * (year + 4716)) + math.floor(30.6001 * (month + 1)) + day + B - 1524.5
-    frac_day = (hour + minute / 60.0 + second / 3600.0) / 24.0
-    return jd + frac_day
+    jd = math.floor(365.25*(year+4716))+math.floor(30.6001*(month+1))+day+(2-(math.floor(year/100))+ math.floor((math.floor(year/100))/4))-1524.5
+    frac_day = (hour+minute/60.0+second/3600.0)/24.0
+    return jd+frac_day
 
 # Helper function to calculate the Greenwich Sidereal Time (GST) in radians
 def gst_from_jd(jd):  
     T = (jd-2451545.0)/36525
-    GMST_seconds = 67310.54841 + (876600*60*60 + 8640184.812866)*T+ 0.093104*T**2- 6.2e-6 * T**3 # ouput in seconds? 
-    gst_rad = math.fmod(GMST_seconds%86400 * OMEGA_EARTH + 2*math.pi, 2*math.pi)
+    GMST_seconds = 67310.54841+(876600*60*60+8640184.812866)*T+0.093104*T**2-6.2e-6*T**3
+    gst_rad = math.fmod(GMST_seconds%86400*OMEGA_EARTH+2*math.pi, 2*math.pi)
     return gst_rad
 
 # Function to convert ECEF to ECI
@@ -50,8 +48,8 @@ def ecef_to_eci(jd, ecef_x_km, ecef_y_km, ecef_z_km):
     gst_rad = gst_from_jd(jd)
     
     # Inverse rotation matrix for ECEF to ECI
-    eci_x_km = ecef_x_km*math.cos(-gst_rad) + ecef_y_km*math.sin(-gst_rad)
-    eci_y_km = ecef_y_km*math.cos(-gst_rad) - ecef_x_km*math.sin(-gst_rad)
+    eci_x_km = ecef_x_km*math.cos(-gst_rad)+ecef_y_km*math.sin(-gst_rad)
+    eci_y_km = ecef_y_km*math.cos(-gst_rad)-ecef_x_km*math.sin(-gst_rad)
     eci_z_km = ecef_z_km
     
     return eci_x_km, eci_y_km, eci_z_km
